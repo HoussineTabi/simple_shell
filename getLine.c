24a4672cb5ef7ea35db06c02701fecb34b47cb06
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-26 08:05:05 -0700

    some_changes

--- getLine.c
+++ getLine.c
@@ -111,7 +111,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 
 /**
  * _getline - gets the next line of input from STDIN
- * @info: parameter struct
+ * @info: parameter struct pass into function
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
  *
